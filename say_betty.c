2ac2db3d9ec3bd86d7d9bb21546b79e20539c17a
anklet1 <flekufelix@gmail.com>
2023-07-24 19:12:41 -0700

    betty here

---
+++ say_betty.c
@@ -0,0 +1,13 @@
+#include <stdio>
+
+
+/**
+ * main - This is an Entry point
+ * Return - always return 0
+ */
+
+int main(void)
+{
+	printf("Betty is the standard test of good code\n");
+	return (0);
+}
